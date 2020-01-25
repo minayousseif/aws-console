918c68fb9a86ded4309a1d3d1e91b0d32295c0ef
Mina Yousseif <mina.yousseif@gmail.com>
2020-01-24 22:18:16 -0500

    adding eslint

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
 
 'use strict';
 
-const AWS   = require('aws-sdk');
+const AWS = require('aws-sdk');
 const axios = require('axios');
-const open  = require('open');
+const open = require('open');
 
-const SIGNIN_URL  = 'https://signin.aws.amazon.com/federation';
+const SIGNIN_URL = 'https://signin.aws.amazon.com/federation';
 const CONSOLE_URL = 'https://console.aws.amazon.com/';
 const TTL_SECONDS = 43200;
 
@@ -18,8 +18,8 @@ const TTL_SECONDS = 43200;
 // https://aws.amazon.com/code/aws-management-console-federation-proxy-sample-use-case/
 
 /*
-  The following AWS STS API call creates a temporary session that returns 
-  a temporary security credentials and a session token. 
+  The following AWS STS API call creates a temporary session that returns
+  a temporary security credentials and a session token.
   The policy grants permissions to the AWS management console.
 */
 const sts = new AWS.STS();
@@ -28,7 +28,7 @@ const federationTokenParams = {
   Name: 'TempUserSession',
   DurationSeconds: TTL_SECONDS,
   Policy: JSON.stringify({
-    Version: '2012-10-17', 
+    Version: '2012-10-17',
     Statement: [
       {
         Effect: 'Allow',
@@ -43,13 +43,12 @@ sts.getFederationToken(federationTokenParams, (err, data) => {
   const defaultErrorMsg = 'Oops, something went wrong. It seems like the call to the federation endpoint did fail';
   if (err) {
     // an error occurred
-    console.error(err);    
-  } 
-  else {
-    /* 
-      On successful response from the aws sdk, we call the federation endpoint, 
-      passing the parameters created earlier and the session information as a JSON block. 
-      The request returns a sign-in token that's valid for 15 minutes. Signing in to 
+    console.error(err);
+  } else {
+    /**
+      On successful response from the aws sdk, we call the federation endpoint,
+      passing the parameters created earlier and the session information as a JSON block.
+      The request returns a sign-in token that's valid for 15 minutes. Signing in to
       the console with the token creates a session that is valid for 12 hours.
     */
     const requestParams = {
@@ -61,15 +60,16 @@ sts.getFederationToken(federationTokenParams, (err, data) => {
         sessionKey: data.Credentials.SecretAccessKey,
         sessionToken: data.Credentials.SessionToken
       }
-    }
-    axios.get(SIGNIN_URL, {params: requestParams } ).then((resp) => {
+    };
+
+    axios.get(SIGNIN_URL, { params: requestParams }).then((resp) => {
       // handle success
       if (resp && resp.status === 200 && resp.data) {
         if (resp.data.SigninToken) {
           /*
             Once we have a sign-in token returned by the federation endpoint,
             We then create the URL to open in the browser, which includes the
-            sign-in token and the URL of the console to open, The "issuer" parameter is optional but 
+            sign-in token and the URL of the console to open, The "issuer" parameter is optional but
             we are not using it since there is no custom identity broker, and we just using our cmd line
           */
           const loginUrl = new URL(SIGNIN_URL);
@@ -89,5 +89,3 @@ sts.getFederationToken(federationTokenParams, (err, data) => {
     });
   }
 });
-
-
